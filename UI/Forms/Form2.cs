53fecfb860cfaf44f9051ec9ce65ac1e8b68d60c
elshaimaa <shimaaazmi8@gmail.com>
2025-03-12 21:40:33 +0200

    login and register validation

--- UI/Forms/Form2.cs
+++ UI/Forms/Form2.cs
@@ -7,13 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Application.Interfaces;
+using DigitalNotesManager.Application.DTOs;
 using DigitalNotesManager.Application.Interfaces;
+using DigitalNotesManager.Application.Services;
+using Infrastructure.Repositories;
+using UI.Forms;
 
 namespace UIPresentation
 {
     public partial class Form2 : Form
     {
         private readonly IUserService _userService;
+        INoteService _noteService;
+        ICategoryService _categoryService;
         public Form2(IUserService userService)
         {
             _userService = userService;
@@ -22,16 +29,24 @@ namespace UIPresentation
 
         private async void RegiserBtn_Click(object sender, EventArgs e)
         {
-            var email = emailTxtBox.Text;
-            var password = passwordTxtBox.Text;
-            var userList = await _userService.GetAllUsersAsync();
-            var user = userList.ToList().Find(xx => xx.Email == email && xx.Password == password);
-            if (user != null)
+            if(string.IsNullOrEmpty(emailTxtBox.Text)|| string.IsNullOrEmpty(passwordTxtBox.Text) ||string.IsNullOrEmpty( UserNameTxtBox.Text) )
             {
-                //TaskManagment taskManagment = new TaskManagment(user.Id);
-                //this.Hide();
-                //taskManagment.ShowDialog();
+                validationMsgLabel.Text = "please complete all required data";
             }
+            else
+            {
+                var email = emailTxtBox.Text;
+                var password = passwordTxtBox.Text;
+                var username = UserNameTxtBox.Text;
+                UserDTO userDTO = new UserDTO() { Email=email,Password=password,Username=username};
+
+                await _userService.AddUserAsync(userDTO);
+
+                MainForm mainform = new MainForm(_noteService, _categoryService);
+                this.Hide();
+                mainform.ShowDialog();
+            }
+            
         }
 
         private void Form2_Load(object sender, EventArgs e)
